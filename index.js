b2f78d092e62b876371e13bff69ca1625cd4eb77
murch1 <18086799+murch1@users.noreply.github.com>
2018-04-03 21:41:41 +1000

    Bug fixes

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ function HttpAutofan(log, config) {
     this.acc_name		 = config["acc_name"];
     // Accessory information
     this.manufacturer    = config["manufacturer"] || "MurchHome";
-    this.model           = config["model"] || "DHT22";
+    this.model           = config["model"] || "MurchAcc";
     this.serial          = config["serial"] || "AAA001";
     // Temperature
     this.temperature     = config["temperature"] || 1;
     this.temp_PV         = config["temp_PV"];
-    this.temp_SP         = config["temp_SP"];
+    this.temp_SPW        = config["temp_SPW"];
+    this.temp_SPR        = config["temp_SPR"];
     this.temp_alarm      = config["temp_alarm"];
     // Humidity
     this.humidity        = config["humidity"] || 0;
@@ -42,22 +43,16 @@ function HttpAutofan(log, config) {
     this.fan_autoSP      = config["fan_autoSP"];
 }
 
+
 function getData(addr, type, callback) {
-    var param = JSON.parse('{"raddr": ' + addr + ',' +
-                 		   '"rtype" : "' + type + '"}');
-	console.log(param);
+    var param = 'raddr=' + addr + '&rtype=' + type;
 	sendData(param, function(res) {
 		callback(res);
 	});
 }
 
-function getSetData(raddr,rtype,waddr,wtype,wmode,write) { 
-	var param = JSON.parse('{"raddr" : ' + raddr + ',' +
-                 		   '"rtype" : "' + rtype + '",' +
-                 		   '"waddr" : ' + waddr + ',' +
-                 		   '"wtype" : "' + wtype + '",' +
-                 		   '"wmode" : "' + wmode + '",' +
-                 		   '"write" : ' + write + '}');
+function getSetData(raddr, rtype, waddr, wtype, wmode, write, callback) {
+	var param = 'write=' + write + '&waddr=' + waddr + '&wtype=' + wtype + '&wmode=' + wmode + '&raddr=' + raddr + '&rtype=' + rtype;
 	sendData(param, function(res) {
 		callback(res);
 	});		
@@ -70,19 +65,20 @@ function sendData(param, callback) {
         	port: '1880',                   // NodeRED port
         	path: '/modbus?' + param,       // HTTP access point + parameters
         	method: 'GET',
+        	timeout: 2500
   	};
 	req = http.request(options, function (res) {
     	res.setEncoding('utf8');
     	res.on('data', function (chunk) {
-		data = JSON.parse(chunk);
-        	callback(data.payload);
+			data = JSON.parse(chunk);
+        	callback(data);
     	});
   	});
-	req.on('error', function(e) {
-		console.log(pilot + ' problem with request: ' + e.message);
-		callback(-1);
-	});
-	req.end();
+		req.on('error', function(e) {
+			console.log('Problem with request: ' + JSON.stringify(e));
+			callback(null);
+		});
+		req.end();
 }
 
 HttpAutofan.prototype = {
@@ -97,11 +93,9 @@ HttpAutofan.prototype = {
         var addr = this.temp_PV;
         var alarm = this.temp_alarm;
         if (addr && alarm) {
-            console.log("Addr = " + addr + " Alarm = " + alarm);
             getData(addr,'h',function(res) {
                 if(res) {
                     var reading = parseFloat(res) / 10.0;
-                    console.log("Temperature = " + reading);
                     callback(null, reading);
                 } else {
                     that.temperature.getCharacteristic(Characteristic.StatusFault).setValue(99);
@@ -109,7 +103,6 @@ HttpAutofan.prototype = {
                 }
             });
         } else {
-            console.log("Addr = " + addr + " Alarm = " + alarm);
             that.temperature.getCharacteristic(Characteristic.StatusFault).setValue(101);
 		    callback(null,0);
         }
@@ -121,11 +114,9 @@ HttpAutofan.prototype = {
         var addr = this.hum_PV;
         var alarm = this.hum_alarm;
         if (addr && alarm) {
-            console.log("Addr = " + addr + " Alarm = " + alarm);
             getData(addr,'h',function(res) {
                 if(res) {
                     var reading = parseInt(res);
-                    console.log("Humidity = " + reading);
                     callback(null, reading);
                 } else {
                     that.humidity.getCharacteristic(Characteristic.StatusFault).setValue(99);
@@ -133,10 +124,189 @@ HttpAutofan.prototype = {
                 }
             });
         } else {
-            console.log("Addr = " + addr + " Alarm = " + alarm);
             that.humidity.getCharacteristic(Characteristic.StatusFault).setValue(101);
 		    callback(null,0);
         }
+    },
+
+    // Fan State Control
+    getFanState: function(callback){
+    	var that = this;
+    	var addr = this.fan_PV;
+        if (addr) {
+            getData(addr,'h',function(res) {
+                if(res !=null || res != undefined) {
+                	var reading = parseInt(res);
+                	if (reading > 0) {
+                		callback(null,1);
+                	} else {
+                	 	callback(null,0);
+                	}
+                }
+            });
+        } else {
+            callback(null,0);
+        }
+    },
+    
+    setFanState: function(fanState, callback){
+    	var that = this;
+		var addr = this.fan_PV;
+		var state;
+        if (addr) {
+            getData(addr,'h',function(res) {
+                if(res) {
+                    	state = 1;
+                    } else {
+                    	state = 0;
+                    }
+				if(state != fanState) {
+					if(fanState) {
+						that.fan.getCharacteristic(Characteristic.RotationSpeed).setValue(2);
+						callback(null);
+					} else {
+						that.fan.getCharacteristic(Characteristic.RotationSpeed).setValue(0);
+						callback(null);
+					}				
+				} else {
+					callback(null);
+				}
+			});
+		} else {
+			callback(null);
+		}
+	},
+
+	// Fan Speed Control
+    getFanSpeed: function(callback){    
+    	var that = this;
+    	var addr = this.fan_PV;
+        if (addr) {
+            getData(addr,'h',function(res) {
+                if(res != null || res != undefined) {
+                    var reading = parseInt(res);
+                    callback(null, reading);
+                } else {
+                	callback(null,0);
+                }
+            });
+        } else {
+            callback(null,0);
+        }
+    },
+    	
+    setFanSpeed: function(fanSpeed, callback){
+    	var that = this;
+		var raddr = this.fan_PV;
+		var waddr;
+		if(fanSpeed > 3) {
+			waddr = this.fan_SP[3];
+		} else if (fanSpeed < 0) {
+			waddr = this.fan_SP[0];
+		} else {
+			waddr = this.fan_SP[fanSpeed];
+		}
+		if (raddr && waddr) {
+			getData(raddr,'h', function(res) {
+				if(fanSpeed != parseInt(res)) {
+					getSetData(raddr,'h',waddr,'c','mom',true,function(res) {
+						that.fan.getCharacteristic(Characteristic.Active).getValue();
+						that.fan.getCharacteristic(Characteristic.RotationSpeed).getValue();
+						that.fan.getCharacteristic(Characteristic.TargetFanState).getValue();
+						callback(null);
+					});				
+				} else {
+					callback(null);
+				}
+			});
+		} else {
+			callback(null);
+		}
+    },
+
+	// Fan Control Mode
+	getAutoMan: function(callback){   
+		var that = this; 
+    	var addr = this.fan_autoPV;
+        if (addr) {
+            getData(addr,'c',function(res) {
+                if(res != null || res != undefined) {
+                	if(res === true) {
+                		callback(null,1);
+                	} else {
+                		callback(null,0);
+                	}
+                } else {
+                	callback(null,0);
+                }
+            });
+        } else {
+            callback(null,0);
+        }
+    },
+	
+    setAutoMan: function(autoManState, callback){
+    	var that = this;
+		var raddr = this.fan_autoPV;
+		var waddr = this.fan_autoSP;
+		var state;
+		if(raddr && waddr) {
+			getData(raddr,'c', function(res) {
+				if(res != null || res != undefined) {
+					if(res === true) {
+						state = 1;
+					} else {
+						state = 0;
+					}
+					if(autoManState != state) {
+						getSetData(raddr,'c',waddr,'c','mom',true,function(res) {
+							that.fan.getCharacteristic(Characteristic.Active).getValue();
+							that.fan.getCharacteristic(Characteristic.RotationSpeed).getValue();
+							that.fan.getCharacteristic(Characteristic.TargetFanState).getValue();
+							callback(null);
+						});
+					}
+				} else {
+					callback(null);
+				}				
+			});
+		} else {
+			callback(null);
+		}
+    },
+    
+	// Fan Temperature Setpoint
+    getTempSP: function(callback){
+    	var that = this;
+    	var addr = this.temp_SPR;
+    	if(addr) {   
+			getData(addr,'h',function(res) {
+				var reading = parseFloat(res) / 10.0;
+				callback(null, reading);	
+			});
+		} else {
+			callback(null, 0);
+		}
+    },
+    
+    setTempSP:  function(targetTemp, callback) {
+    	var that = this;
+		var raddr = this.temp_SPR;
+		var waddr = this.temp_SPW;
+		if(raddr && waddr) {
+			getData(raddr,'h',function(res) {
+				if(targetTemp != (parseFloat(res) / 10.0)) {
+					getSetData(raddr,'h',waddr,'h','set',parseInt(targetTemp * 10),function(res) {
+						that.fan.getCharacteristic(Characteristic.TargetTemperature).getValue();
+						callback(null);
+					});
+				} else {
+					callback(null);
+				}
+			});
+		} else {
+			callback(null);
+		}
     },
     
 
@@ -163,7 +333,7 @@ HttpAutofan.prototype = {
         }
         
         if (this.humidity) {
-            var humidityService = new Service.HumiditySensor(this.acc_name + 'Humidity');
+            var humidityService = new Service.HumiditySensor(this.acc_name + ' Humidity');
             humidityService
                 .getCharacteristic(Characteristic.CurrentRelativeHumidity)
                 .setProps({ minValue: 0, maxValue: 100 })
@@ -174,6 +344,28 @@ HttpAutofan.prototype = {
             this.humidity = humidityService;
             services.push(this.humidity);
         }
+        
+        if(this.fan) {
+			var fanService = new Service.Fanv2(this.acc_name + ' Fan');
+			fanService
+				.getCharacteristic(Characteristic.Active)
+				.on('get', this.getFanState.bind(this))
+				.on('set', this.setFanState.bind(this))
+			fanService
+				.getCharacteristic(Characteristic.RotationSpeed)
+				.on('get', this.getFanSpeed.bind(this))
+				.on('set', this.setFanSpeed.bind(this))
+			fanService
+				.getCharacteristic(Characteristic.TargetFanState)
+				.on('get', this.getAutoMan.bind(this))
+				.on('set', this.setAutoMan.bind(this))
+			fanService
+				.getCharacteristic(Characteristic.TargetTemperature)
+				.on('get', this.getTempSP.bind(this))
+				.on('set', this.setTempSP.bind(this))
+			this.fan = fanService;
+			services.push(this.fan);
+		}
 
         return services;
     }
