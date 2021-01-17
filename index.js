a54c6d0fe373c848afa207920a2feaefa1e5872c
murch1 <18086799+murch1@users.noreply.github.com>
2021-01-17 15:13:03 +1000

    Update index.js
    Bug fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var superagentCache = require("superagent-cache-plugin")(cache);
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-http-autofan", "http-autofan", HttpAutofan);
+    homebridge.registerAccessory("homebridge-httpautofan", "http-autofan", HttpAutofan);
 }
 
 function HttpAutofan(log, config) {
